4168d0f2e6c56237902153c43e1d6659bd7135f2
lfhwnqe <464385492@qq.com>
2019-05-04 11:14:21 +0800

    订单模块：增加订单列表首页统计，订单条件查询增加字段待调试后端

--- src/views/order_list/index.vue
+++ src/views/order_list/index.vue
@@ -1,57 +1,82 @@
 <template>
   <div class="order-list ui-container ui-pa-1">
-    <mu-container ref="container" class="demo-loadmore-content">
-      <mu-load-more @refresh="refresh" :refreshing="refreshing" :loading="loading" @load="load">
-        <mu-list>
-          <template v-for="(item,index) in orderList">
-            <mu-list-item :ripple="false" v-if="index===dateIndex[new Date(item.saleTime).toLocaleDateString()]">
-              <mu-list-item-title>{{ item.saleTime|date|empty }}</mu-list-item-title>
+    <mu-load-more @refresh="refresh" :refreshing="refreshing" :loading="loading" @load="load">
+      <mu-list>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-content>
+            <mu-list-item-title>数据统计</mu-list-item-title>
+            <mu-list-item-sub-title>
+              订单数：{{ total }}
+            </mu-list-item-sub-title>
+            <mu-list-item-sub-title>总收入：{{ totalPrice|currency }}</mu-list-item-sub-title>
+          </mu-list-item-content>
+          <!--<mu-list-item-action>-->
+          <!--<mu-icon value="search" @click="openBotttomSheet"></mu-icon>-->
+          <!--</mu-list-item-action>-->
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <template v-for="(item,index) in orderList">
+          <mu-list-item :ripple="false" v-if="index===dateIndex[new Date(item.saleTime).toLocaleDateString()]">
+            <mu-list-item-title>{{ item.saleTime|date|empty }}</mu-list-item-title>
 
-              <mu-list-item-action>
-                <mu-icon value="search" @click="openBotttomSheet"></mu-icon>
-              </mu-list-item-action>
-            </mu-list-item>
+            <mu-list-item-action>
+              <mu-icon value="search" @click="openBotttomSheet"></mu-icon>
+            </mu-list-item-action>
+          </mu-list-item>
 
-            <mu-list-item @click="goDetail(index)" button :ripple="false">
-              <mu-list-item-content>
-                <mu-list-item-title>订单总价：{{ item.ordersTotalPrice|currency|empty }}</mu-list-item-title>
-                <mu-list-item-sub-title>备注：{{ item.remark|empty }}</mu-list-item-sub-title>
-              </mu-list-item-content>
-              <mu-list-item-action>
-                <mu-icon value="more_horiz"></mu-icon>
-              </mu-list-item-action>
-            </mu-list-item>
-          </template>
-        </mu-list>
-      </mu-load-more>
 
-      <mu-dialog width="360" transition="slide-bottom" fullscreen :open.sync="botttomSheet">
-        <mu-appbar color="primary" title="搜索">
-          <mu-button slot="left" icon @click="botttomSheet=false">
-            <mu-icon value="close"></mu-icon>
-          </mu-button>
-          <mu-button slot="right" flat @click="search">
-            搜索
-          </mu-button>
-        </mu-appbar>
-        <div class="ui-pa-1">
-          <mu-form ref="ordersForm" :model="form" class="mu-demo-form" label-position="left"
-            label-width="100">
-            <mu-form-item prop="phone" help-text="输入电话号码" label="电话号码" :rules="phoneRules">
-              <mu-auto-complete @input="searchPhoneNumber" :data="phoneLists" v-model="form.phoneNumber"
-                avatar>
-              </mu-auto-complete>
-            </mu-form-item>
-            <mu-form-item help-text="选择销售员" label="销售员" :rules="phoneRules">
-              <mu-select v-model="form.saleBy" full-width>
-                <mu-option v-for="option,index in saleByList" :key="index" :label="option.label"
-                  :value="option.value"></mu-option>
-              </mu-select>
-            </mu-form-item>
-          </mu-form>
-        </div>
-      </mu-dialog>
-    </mu-container>
+          <mu-list-item @click="goDetail(index)" button :ripple="false">
+            <mu-list-item-content>
+              <mu-list-item-title>订单总价：{{ item.ordersTotalPrice|currency|empty }}</mu-list-item-title>
+              <mu-list-item-sub-title>备注：{{ item.remark|empty }}</mu-list-item-sub-title>
+            </mu-list-item-content>
+            <mu-list-item-action>
+              <mu-icon value="more_horiz"></mu-icon>
+            </mu-list-item-action>
+          </mu-list-item>
+          <mu-divider></mu-divider>
+        </template>
+      </mu-list>
+    </mu-load-more>
+
+    <mu-dialog width="360" transition="slide-bottom" fullscreen :open.sync="botttomSheet">
+      <mu-appbar color="primary" title="搜索">
+        <mu-button slot="left" icon @click="botttomSheet=false">
+          <mu-icon value="close"></mu-icon>
+        </mu-button>
+        <mu-button slot="right" flat @click="search">
+          搜索
+        </mu-button>
+      </mu-appbar>
+      <div class="ui-pa-1">
+        <mu-form ref="ordersForm" :model="form" class="mu-demo-form" label-position="left"
+          label-width="100">
+          <mu-form-item prop="phone" help-text="输入电话号码" label="电话号码" :rules="phoneRules">
+            <mu-auto-complete @input="searchPhoneNumber" :data="phoneLists" v-model="form.phoneNumber"
+              avatar>
+            </mu-auto-complete>
+          </mu-form-item>
+          <mu-form-item help-text="选择销售员" label="销售员" :rules="phoneRules">
+            <mu-select v-model="form.saleBy" full-width>
+              <mu-option v-for="option,index in saleByList" :key="index" :label="option.userLabel"
+                :value="option._id"></mu-option>
+            </mu-select>
+          </mu-form-item>
+          <mu-form-item help-text="选择起始时间" label="起始时间">
+            <mu-date-input v-model="form.fromTime" prop="fromTime" container="bottomSheet" type="dateTime"
+              actions></mu-date-input>
+          </mu-form-item>
+          <mu-form-item help-text="选择截止时间" label="截止时间">
+            <mu-date-input v-model="form.endTime" prop="endTime" container="bottomSheet" type="dateTime"
+              actions></mu-date-input>
+          </mu-form-item>
+          <mu-form-item>
+            <mu-button color="primary" @click="submit">查询</mu-button>
+            <mu-button @click="clear">重置</mu-button>
+          </mu-form-item>
+        </mu-form>
+      </div>
+    </mu-dialog>
   </div>
 </template>
 
@@ -76,8 +101,12 @@
         form: {
           pageSize: 20,
           pageNum: 1,
-          phoneNumber: ''
+          phoneNumber: '',
+          fromTime: '',
+          endTime: ''
         },
+        totalPrice: 0,
+        total: 0,
         totalPage: 0,
         phoneRules: [{
           validate: (val) => {
@@ -90,6 +119,16 @@
       };
     },
     methods: {
+      submit() {},
+      clear() {
+        this.form = {
+          pageSize: 20,
+          pageNum: 1,
+          phoneNumber: '',
+          fromTime: '',
+          endTime: ''
+        };
+      },
       search() {},
       openBotttomSheet() {
         this.botttomSheet = true;
@@ -102,7 +141,6 @@
       },
       getSaleByList() {
         api.getSaleByList().then(res => {
-          console.log(res);
           this.saleByList = res;
         });
       },
@@ -117,6 +155,8 @@
           this.form.pageSize = res.pageSize;
           this.form.pageNum = res.pageNum;
           this.totalPage = res.totalPage;
+          this.total = res.total;
+          this.totalPrice = res.totalPrice;
         }).finally(_ => {
           this.refreshing = false;
         });
@@ -130,6 +170,8 @@
           this.form.pageSize = res.pageSize;
           this.form.pageNum = res.pageNum;
           this.totalPage = res.totalPage;
+          this.total = res.total;
+          this.totalPrice = res.totalPrice;
         }).finally(_ => {
           this.loading = false;
         });
